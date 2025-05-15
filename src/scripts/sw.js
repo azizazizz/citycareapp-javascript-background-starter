06f0add320f7cfcc23e3d0a436d46c4d192089ea
Nur Aziz Raihan <nurazispbgp@gmail.com>
2025-05-15 18:55:03 +0700

    sudah bisaa push notif

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,12 +1,12 @@
-EventListener('push', (event) => {
+self.addEventListener('push', (event) => {
   console.log('Service worker pushing...');
- 
+
   async function chainPromise() {
     const data = await event.data.json();
     await self.registration.showNotification(data.title, {
       body: data.options.body,
     });
   }
- 
+
   event.waitUntil(chainPromise());
 });
